3b257617dffd8965cbf14614ed2bc6f101f08919
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-04 18:16:04 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -1,13 +1,9 @@
-let esperandoRespuesta = {};
-
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-  const name = await conn.getName(m.sender);
+  const regex = /^hola$/i;
 
   if (command === 'prueva') {
-    esperandoRespuesta[m.sender] = true;
-    await conn.sendMessage(m.chat, { text: 'Mensaje de prueba, responda con *Hola:* hola' }, { quoted: m });
-  } else if (esperandoRespuesta[m.sender] && text.toLowerCase() === 'hola') {
-    esperandoRespuesta[m.sender] = false;
+    await conn.sendMessage(m.chat, { text: 'Mensaje de prueba, cite este mensaje con "Hola"' }, { quoted: m });
+  } else if (m.quoted && m.quoted.fromMe && regex.test(m.text)) {
     await conn.sendMessage(m.chat, { text: '¡Hola! ¿En qué puedo ayudarte?' }, { quoted: m });
   }
 };
