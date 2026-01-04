2e72ba32ca58a19308dd19524b9ab1f6f2056b77
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-04 18:12:21 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -1,16 +1,17 @@
+let esperandoRespuesta = {};
+
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
   const name = await conn.getName(m.sender);
-  const regex = /^hola$/i;
 
-  if (m.quoted && regex.test(m.quoted.text)) {
-    // Si el mensaje es una respuesta al mensaje del bot y coincide con "hola"
-    await conn.sendMessage(m.chat, { text: '¡Hola! ¿Cómo estás?' }, { quoted: m });
-  } else {
-    // Si no es una respuesta o no coincide con "hola"
-    let xd = `Mensaje de prueba, responda con *Hola:* hola`;
-    await conn.sendMessage(m.chat, { text: xd }, { quoted: m });
+  if (command === 'prueva') {
+    esperandoRespuesta[m.sender] = true;
+    await conn.sendMessage(m.chat, { text: 'Mensaje de prueba, responda con *Hola:* hola' }, { quoted: m });
+  } else if (esperandoRespuesta[m.sender] && text.toLowerCase() === 'hola') {
+    esperandoRespuesta[m.sender] = false;
+    await conn.sendMessage(m.chat, { text: '¡Hola! ¿En qué puedo ayudarte?' }, { quoted: m });
   }
 };
 
 handler.command = ['prueva'];
+
 export default handler;
