4b38c59191b07d54546cd040fe947c497a09cdbf
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-04 23:00:30 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -1,14 +1,12 @@
-
 import fetch from 'node-fetch'
 
-let handler = async (m, { conn, usedPrefix, command, text, args }) => {
+let handler = async (m, { conn, usedPrefix, command, text, args, command }) => {
 if (!global.db.data.chats[m.chat].fRpg && m.isGroup) {
 return conn.sendMessage(m.chat, { text: `✦ Los comandos de *[ RPG ]* estan desactivados.\n- Un administrador puede activarlo con: *#fc-rpg on*` }, { quoted: m })
 }
 
-let isDatos = /--date|date$/i.test(text || args[0])
-let isRecurs = /--recs|recursos$/i.test(text || args[0])
-
+//let isDatos = /--date|date$/i.test(text || args[0])
+//let isRecurs = /--recs|recursos$/i.test(text || args[0])
 const thumb = Buffer.from(await (await fetch(`https://files.catbox.moe/o63bvv.jpg`)).arrayBuffer())
 let mentionedJid = await m.mentionedJid
 let who = mentionedJid[0] ? mentionedJid[0] : m.quoted ? await m.quoted.sender : m.sender
@@ -23,8 +21,6 @@ let misocial = user.misocial || '✘ (#red+)'
 
 let monedas = user.torucoin || 0
 let experiencia = user.toruexp || 0
-let nivel = user.nivele || 0
-let estado = user.rangos || 0
 let taw = user.tawbot || 0
 let taw2 = user.puntos || 0
 let taw3 = user.torucora || 0
@@ -36,21 +32,23 @@ let taw8 = user.boletos || 0
 let taw9 = user.torupesc || 0
 let taw10 = user.torullave || 0
 let taw11 = user.cupones || 0
+
+if (!args[0]) {
 const texto = `\t\t【  *P E R F I L*  】
 - Perfil de @${name}
 
 > 々 *Detalles:*
-𔓕 ${currency} : *${monedas.toLocaleString()}*
-✩ ${currency2} : *${experiencia.toLocaleString()}*
-🜲 Rango : *#${estado.toLocaleString()}*
-𖡛 Nivel : *lvl_${nivel.toLocaleString()}*
+𔓕 ${currency} : *${torucoin.toLocaleString()}*
+✩ ${currency2} : *${toruexp.toLocaleString()}*
+🜲 Rango : *#${rangos.toLocaleString()}*
+𖡛 Nivel : *lvl_${nivele.toLocaleString()}*
 
 📍  Puede usar *${usedPrefix + command} --date* o *--recs* para ver tus datos o recursos.
 
 > ${textbot}`
 await conn.sendMessage(m.chat, { text: texto, mentions: [m.sender], contextInfo: { externalAdReply: { title: "⫶☰  P E R F I L", body: botname, thumbnail: thumb, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
 
-if (isDatos) {
+} else if (args[0] === "--date" && mentionedJid) {
 let datos = `\t\t【  *P E R F I L*  】
 > ${description}
 
@@ -63,8 +61,8 @@ let datos = `\t\t【  *P E R F I L*  】
 - ${misocial}
 
 > ${textbot}`
-return await conn.sendMessage(m.chat, { text: datos, mentions: [m.sender], contextInfo: { externalAdReply: { title: "⫶☰  P E R F I L", body: `👋🏻 ¡Edita tu perfil con #myp!`, thumbnail: thumb, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
-} else if (isRecurs) {
+return conn.sendMessage(m.chat, { text: datos, mentions: [m.sender], contextInfo: { externalAdReply: { title: "⫶☰  P E R F I L", body: `👋🏻 ¡Edita tu perfil con #myp!`, thumbnail: thumb, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
+} else if (args[0] === "--recs" && mentionedJid) {
 let recursos = `\t\t【  *R E C U R S O S*  】
 - Recursos de @${name} 🎒
 
@@ -88,7 +86,7 @@ let recursos = `\t\t【  *R E C U R S O S*  】
 🎟️ Cupones : *${taw11.toLocaleString()}*
 
 > ${textbot}`
-return await conn.sendMessage(m.chat, { text: recursos, mentions: [m.sender], contextInfo: { externalAdReply: { title: "⫶☰  P E R F I L", body: `🎒 Todos tus recursos aqui.`, thumbnail: thumb, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
+return conn.sendMessage(m.chat, { text: recursos, mentions: [m.sender], contextInfo: { externalAdReply: { title: "⫶☰  P E R F I L", body: `🎒 Todos tus recursos aqui.`, thumbnail: thumb, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
 } else {
 let noXd = `📍  No hay otra categoría por ver.`
 return conn.sendMessage(m.chat, { text: noXd }, { quoted: m })
@@ -100,5 +98,7 @@ handler.group = true
 
 export default handler
 
+
+                                                                 
 
   
