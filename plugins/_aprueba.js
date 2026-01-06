519b2b76aaa81c1337d64509622e2272f1a576b7
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-06 16:35:59 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -5,6 +5,7 @@ if (!text) return conn.sendMessage(m.chat, { text: `Escribe para crear una image
 await m.react('⏳')
 const res = await global.sendOptishield({ type: "text2img", text: text})
 console.log(res)
+await conn.sendMessage(m.chat, { image: { url: res.result.img }, caption: "image-text2img.png" }, { quoted: m })
 await m.react('✅')
 
 }
