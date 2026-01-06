3fb446ddf8e9545825c38201214693a50166ba8a
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-06 16:09:59 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 if (!text) return conn.sendMessage(m.chat, { text: `Escribe para crear una imagen.` }, { quoted: m })
 await m.react('⏳')
-const res = await sendOptishield({ type, text})
+const res = await global.sendOptishield({ type, text})
 console.log(res)
 await m.react('✅')
 
