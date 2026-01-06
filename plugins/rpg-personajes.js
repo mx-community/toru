5606b1ce586f990794f2c6ea868077d6d1588c88
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-06 01:43:47 -0300

    Add files via upload

---
+++ plugins/rpg-personajes.js
@@ -0,0 +1,19 @@
+const personajes = {
+'1': {
+nombre: 'Endeavor',
+rareza: 'común',
+valor: 10
+},
+'2': {
+nombre: 'Takeda',
+rareza: 'rara',
+valor: 10
+},
+'3': {
+nombre: 'Guts',
+rareza: 'épica',
+valor: 10
+}
+};
+
+export default { personajes };
---
+++ plugins/rpg-personajes_shop.js
@@ -0,0 +1,48 @@
+import { personajes } from './rpg-coleccion.js';
+
+let handler = async (m, { conn, usedPrefix, command, args }) => {
+let users = global.db.data.users;
+let userId = m.sender;
+
+if (command === 'shop!') {
+if (args.length !== 1) {
+m.reply(`Uso: ${usedPrefix}shop! <nombre del personaje>`);
+return;
+}
+
+let personajeId = args[0].toLowerCase();
+let personaje = Object.values(personajes).find(p => p.nombre.toLowerCase() === personajeId);
+
+if (!personaje) {
+m.reply(`El personaje "${personajeId}" no existe.`);
+return;
+}
+
+if (users[userId].personajes && users[userId].personajes[personaje.nombre]) {
+m.reply(`Ya tienes a ${personaje.nombre}.`);
+return;
+}
+
+if (users[userId].boletos < personaje.valor) {
+m.reply(`No tienes suficientes boletos para comprar a ${personaje.nombre}. Necesitas ${personaje.valor} boletos.`);
+return;
+}
+
+if (!users[userId].personajes) {
+users[userId].personajes = {};
+}
+
+users[userId].personajes[personaje.nombre] = {
+nombre: personaje.nombre,
+rareza: personaje.rareza,
+valor: personaje.valor,
+};
+
+users[userId].boletos -= personaje.valor;
+m.reply(`¡Felicidades! Has comprado a ${personaje.nombre} por ${personaje.valor} boletos.`);
+}
+};
+
+handler.command = ['shop!'];
+export default handler;
+
