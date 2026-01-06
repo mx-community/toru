65ac2db113e4180166a947939485bf3146a0ed0d
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-06 02:26:10 -0300

    Update rpg-personajes_shop.js

--- plugins/rpg-personajes_shop.js
+++ plugins/rpg-personajes_shop.js
@@ -1,24 +1,43 @@
-import { personajes } from './plugins/rpg-personajes.js';
+import fs from 'fs';
+import { personajes } from './rpg-personajes.js';
+
+const dbFile = './database.json';
+const dbData = fs.existsSync(dbFile) ? JSON.parse(fs.readFileSync(dbFile)) : {};
+
+global.db = {
+data: dbData,
+save: () => {
+fs.writeFileSync(dbFile, JSON.stringify(dbData, null, 2));
+}
+};
 
 let handler = async (m, { conn, usedPrefix, command, args }) => {
 let users = global.db.data.users;
 let userId = m.sender;
 
+if (!users[userId]) {
+users[userId] = {
+boletos: 0,
+personajes: {}
+};
+global.db.save();
+}
+
 if (command === 'shop!') {
-if (args.length !== 1) {
-m.reply(`Uso: ${usedPrefix}shop! <nombre del personaje>`);
+if (args.length < 1) {
+m.reply(`Uso: ${usedPrefix}comprar <nombre del personaje>`);
 return;
 }
 
-let personajeId = args[0].toLowerCase();
-let personaje = Object.values(personajes).find(p => p.nombre.toLowerCase() === personajeId);
+let nombrePersonaje = args.join(' ').toLowerCase();
+let personaje = Object.values(personajes).find(p => p.nombre.toLowerCase().includes(nombrePersonaje));
 
 if (!personaje) {
-m.reply(`El personaje "${personajeId}" no existe.`);
+m.reply(`El personaje "${nombrePersonaje}" no existe.`);
 return;
 }
 
-if (users[userId].personajes && users[userId].personajes[personaje.nombre]) {
+if (users[userId].personajes[personaje.nombre]) {
 m.reply(`Ya tienes a ${personaje.nombre}.`);
 return;
 }
@@ -28,10 +47,6 @@ m.reply(`No tienes suficientes boletos para comprar a ${personaje.nombre}. Neces
 return;
 }
 
-if (!users[userId].personajes) {
-users[userId].personajes = {};
-}
-
 users[userId].personajes[personaje.nombre] = {
 nombre: personaje.nombre,
 rareza: personaje.rareza,
@@ -39,10 +54,11 @@ valor: personaje.valor,
 };
 
 users[userId].boletos -= personaje.valor;
+global.db.save();
 m.reply(`¡Felicidades! Has comprado a ${personaje.nombre} por ${personaje.valor} boletos.`);
 }
 };
 
 handler.command = ['shop!'];
 export default handler;
-
+  
