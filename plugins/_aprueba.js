c9e622b4b5f525faa2690b86ff6235a25173d885
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-06 15:20:17 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -1,32 +1,21 @@
-let handler = async (m, { conn, usedPrefix, text, command, args }) => {
-  let texto = await m.mentionedJid;
-  let userId = texto?.length > 0 ? texto[0] : (m.quoted ? m.quoted.sender : m.sender);
+import fetch from 'node-fetch'
+import axios from 'axios'
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+if (!text) return conn.sendMessage(m.chat, { text: `Escribe para crear una imagen.` }, { quoted: m })
+//const apiUrl = `https://optishield.uk//api?type=img2img?text=${text}&apikey=${apikey}`
+try {
+await m.react('⏳')
+let apikey = "ebe2e764b8a003d278472b711498aec7"
+const url = `https://optishield.uk/api/?type=img2img?text=${text}&apikey=${apikey}`
+const res = await fetch(url)
+const data = await res.json()
+await conn.sendMessage(m.chat, { image: { url: data.img }, { quoted: m })
+//await conn.sendMessage(m.chat, {image: Buffer.from(response.data)}, {quoted: m})
+await m.react('✅')
 
-  if (!global.db.data.users[userId]) {
-    m.reply(`El usuario ${userId} no está en la base de datos.`);
-    return;
-  }
+}
 
-  const user = global.db.data.users[userId];
-  let name = user.name || (await conn.getName(userId).catch(() => userId.split('@')[0]));
+handler.command = ['xd']
 
-  if (args.length < 2) {
-    m.reply(`Uso: ${usedPrefix}give! @usuario boletos <cantidad>`);
-    return;
-  }
-
-  let cantidad = parseInt(args[1]);
-  if (isNaN(cantidad) || cantidad <= 0) {
-    m.reply(`La cantidad debe ser un número positivo.`);
-    return;
-  }
-
-  user.boletos = (user.boletos || 0) + cantidad;
-  global.db.save();
-  m.reply(`Se han agregado ${cantidad} boletos a ${name}. Ahora tiene ${user.boletos} boletos.`);
-};
-
-handler.command = ['give!'];
-handler.rowner = true;
-export default handler;
-  
+export default handler
+                                
