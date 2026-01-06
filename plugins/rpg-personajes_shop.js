e359bf4604fadc23300dcd6ea0492cc1205df045
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-06 04:07:06 -0300

    Update rpg-personajes_shop.js

--- plugins/rpg-personajes_shop.js
+++ plugins/rpg-personajes_shop.js
@@ -1,48 +1,55 @@
 import { personajes } from './rpg-personajes.js';
 
 let handler = async (m, { conn, usedPrefix, command, args }) => {
-let users = global.db.data.users;
-let userId = sender;
+  let users = global.db.data.users;
+  let userId = m.sender;
 
-if (command === 'shop!') {
-if (args.length !== 1) {
-conn.reply(`Uso: ${usedPrefix}comprar <nombre del personaje>`);
-return;
-}
+  if (command === 'comprar') {
+    if (args.length !== 1) {
+      conn.reply(`Uso: ${usedPrefix}comprar <nombre del personaje>`);
+      return;
+    }
 
-let personajeId = args[0].toLowerCase();
-let personaje = Object.values(personajes).find(p => p.nombre.toLowerCase() === personajeId);
+    let personajeId = args[0].toLowerCase();
+    let personaje = Object.values(personajes).find(p => p.nombre.toLowerCase() === personajeId);
 
-if (!personaje) {
-conn.reply(`El personaje "${personajeId}" no existe.`);
-return;
-}
+    if (!personaje) {
+      conn.reply(`El personaje "${personajeId}" no existe.`);
+      return;
+    }
 
-if (users[userId].personajes && users[userId].personajes[personaje.nombre]) {
-conn.reply(`Ya tienes a ${personaje.nombre}.`);
-return;
-}
+    if (!users[userId]) {
+      users[userId] = {
+        personajes: {},
+        boletos: 0
+      };
+    }
 
-if (users[userId].boletos < personaje.valor) {
-conn.reply(`No tienes suficientes boletos para comprar a ${personaje.nombre}. Necesitas ${personaje.valor} boletos.`);
-return;
-}
+    if (users[userId].personajes && users[userId].personajes[personaje.nombre]) {
+      conn.reply(`Ya tienes a ${personaje.nombre}.`);
+      return;
+    }
 
-if (!users[userId].personajes) {
-users[userId].personajes = {};
-}
+    if (users[userId].boletos < personaje.valor) {
+      conn.reply(`No tienes suficientes boletos para comprar a ${personaje.nombre}. Necesitas ${personaje.valor} boletos.`);
+      return;
+    }
 
-users[userId].personajes[personaje.nombre] = {
-nombre: personaje.nombre,
-rareza: personaje.rareza,
-valor: personaje.valor
+    if (!users[userId].personajes) {
+      users[userId].personajes = {};
+    }
+
+    users[userId].personajes[personaje.nombre] = {
+      nombre: personaje.nombre,
+      rareza: personaje.rareza,
+      valor: personaje.valor
+    };
+
+    users[userId].boletos -= personaje.valor;
+    conn.reply(`¡Felicidades! Has comprado a ${personaje.nombre} por ${personaje.valor} boletos.`);
+  }
 };
 
-users[userId].boletos -= personaje.valor;
-conn.reply(`¡Felicidades! Has comprado a ${personaje.nombre} por ${personaje.valor} boletos.`);
-}
-};
-
-handler.command = ['shop!'];
+handler.command = ['comprar'];
 export default handler;
-  
+
