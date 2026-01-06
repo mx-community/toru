50bdfd25b58af896b2c4318dd508583d3621c2c9
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-06 15:28:53 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -3,13 +3,12 @@ import axios from 'axios'
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 if (!text) return conn.sendMessage(m.chat, { text: `Escribe para crear una imagen.` }, { quoted: m })
 //const apiUrl = `https://optishield.uk//api?type=img2img?text=${text}&apikey=${apikey}`
-try {
 await m.react('⏳')
 let apikey = "ebe2e764b8a003d278472b711498aec7"
 const url = `https://optishield.uk/api/?type=img2img?text=${text}&apikey=${apikey}`
 const res = await fetch(url)
 const data = await res.json()
-await conn.sendMessage(m.chat, { image: { url: data.img }, { quoted: m })
+await conn.sendMessage(m.chat, { image: { url: data.img } }, { quoted: m })
 //await conn.sendMessage(m.chat, {image: Buffer.from(response.data)}, {quoted: m})
 await m.react('✅')
 
