f6871c8540ee6019f01a1c6404d2b95b10372372
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-06 16:07:46 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -2,14 +2,9 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 if (!text) return conn.sendMessage(m.chat, { text: `Escribe para crear una imagen.` }, { quoted: m })
-//const apiUrl = `https://optishield.uk//api?type=img2img?text=${text}&apikey=${apikey}`
 await m.react('⏳')
-let apikey = "ebe2e764b8a003d278472b711498aec7"
-const url = `https://optishield.uk/api/?type=img2img?text=${text}&apikey=${apikey}`
-const res = await fetch(url)
-//const data = await res.json()
-await conn.sendMessage(m.chat, { image: { url: res.img } }, { quoted: m })
-//await conn.sendMessage(m.chat, {image: Buffer.from(response.data)}, {quoted: m})
+const res = await sendOptishield({ type, text})
+console.log(res)
 await m.react('✅')
 
 }
