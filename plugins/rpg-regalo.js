872b1e348ac3b69bedd95730c49c9f60dbb933ac
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-07 09:51:26 -0300

    Create rpg-regalo.js

---
+++ plugins/rpg-regalo.js
@@ -0,0 +1,65 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, usedPrefix, command }) => {
+if (!global.db.data.chats[m.chat].fRpg && m.isGroup) {
+return conn.sendMessage(m.chat, { text: `✦ Los comandos de *[ RPG ]* estan desactivados.\n- Un administrador puede activarlo con: *#fc-rpg on*` }, { quoted: m })
+}
+let user = global.db.data.users[m.sender]
+ const thumbNo = Buffer.from(await (await fetch(`https://files.catbox.moe/e9h7b2.jpg`)).arrayBuffer())
+ 
+if (user.toruregal >= 1) {
+const thumb = Buffer.from(await (await fetch(`https://files.catbox.moe/vj49qn.jpg`)).arrayBuffer())
+let monedas, experiencia, corazones, velas, piesas, boletoss, fragmentos, puntoss
+monedas = Math.floor(Math.random() * 60) 
+experiencia = Math.floor(Math.random() * 60) 
+puntoss = Math.floor(Math.random() * 80) 
+fragmentos = Math.floor(Math.random() * 80) 
+corazones = Math.floor(Math.random() * 15) 
+velas = Math.floor(Math.random() * 15)  
+piesas = Math.floor(Math.random() * 15) 
+boletoss = Math.floor(Math.random() * 15) 
+user.torucoin += monedas
+user.toruexp += experiencia
+user.torucora += corazones
+user.toruvela += velas
+user.tawbot += fragmentos 
+user.puntos += puntoss
+user.torupiesa += piesas
+user.boletos += boletoss
+user.torullave += 3
+let cofreXd = `\t〩  *R E G A L O  :  R P G*
+> ¡Abriste un cofre, reclama tu recompensa!
+
+⚶ *Balance:*
+\t𔓕 ${currency}  :  *+${monedas.toLocaleString()}*
+\t✩ ${currency2}  :  *+${experiencia.toLocaleString()}*
+
+⚶ *Necesario:*
+\t💠 Fragmentos : *+${fragmentos.toLocaleString()}*
+\t🌀 Puntos : *+${puntoss.toLocaleString()}*
+
+⚶ *Recursos:*
+\t❤️ Corazones : *+${corazones.toLocaleString()}*
+\t🕯️ Velas : *+${velas.toLocaleString()}*
+\t🧩 Piesas : *+${piesas.toLocaleString()}*
+\t🧧 Boletos : *+${boletoss.toLocaleString()}*
+\t🗝️ Llaves : *+3*
+
+> 🎁 _Consigue mas regalos para mas recompensas._`
+await conn.sendMessage(m.chat, { text: cofreXd, mentions: [m.sender], contextInfo: { externalAdReply: { title: "々  C O F R E  々", body: botname, thumbnail: thumb, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
+user.toruregal -= 1
+} else {
+let noXd = `No tienes *[ 🎁 Regalos ]* para abrir.\n- Puedes comprar en la tienda usando *${usedPrefix}shop*.`
+return await conn.sendMessage(m.chat, { text: noXd, mentions: [m.sender], contextInfo: { externalAdReply: { title: "🎁 Sin regalos para abrir", body: botname, thumbnail: thumbNo, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
+// conn.sendMessage(m.chat, { text: noXd }, { quoted: m })
+ }
+}
+
+handler.command = ['coffer', 'cofre']
+handler.group = true
+
+export default handler
+
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]
+   }
+  
