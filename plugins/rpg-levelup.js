191792dbe3db288461528c33cd2548b3e3f4aba5
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-07 03:33:45 -0300

    Update rpg-levelup.js

--- plugins/rpg-levelup.js
+++ plugins/rpg-levelup.js
@@ -10,9 +10,9 @@ if (!args[0]) {
 let estado = `· ┄ · ⊸ 𔓕 *Nivel  :  Rango*
 > ¡Aqui tienes la información de tu nivel y rango!
 
-> *Rango y Nivel:*
-🜲 Rango : *#${user.rangos}*
+> *Nivel y Rango:*
 𖡛 Nivel : *lvl_${user.nivele}*
+🜲 Rango : *#${user.rangos}*
 
 > Usa los siguientes comandos para subir de nivel o rango.
 *${usedPrefix + command}* --up
@@ -20,7 +20,7 @@ let estado = `· ┄ · ⊸ 𔓕 *Nivel  :  Rango*
 imagen = Buffer.from(await (await fetch(`https://files.catbox.moe/awfqp3.jpg`)).arrayBuffer())
 await conn.sendMessage(m.chat, { text: estado, mentions: [m.sender], contextInfo: { externalAdReply: { title: "〩 Nivel - Actual 〩", body: botname, thumbnail: imagen, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
 } else if (args[0] === "--up") {
-if (user.tawbot >= 300) {
+if (user.tawbot >= 100) {
 nivelado = 1
 llaves = Math.floor(Math.random() * 2)
 monedas = Math.floor(Math.random() * 25)
@@ -37,17 +37,17 @@ let respNivel = `· ┄ · ⊸ 𔓕 *New  :  Level*
 ❒ *${currency}* : +${monedas.toLocaleString()}
 ❒ *${currency2}* : +${experiencia.toLocaleString()}
 
-> Consigue *[ 💠 300 Fragmentos ]* para subir de nivel y recibir recompensas.`
+> Consigue *[ 💠 100 Fragmentos ]* para subir de nivel y recibir recompensas.`
 imagen = Buffer.from(await (await fetch(`https://files.catbox.moe/10bj4k.jpg`)).arrayBuffer())
 await await conn.sendMessage(m.chat, { text: respNivel, mentions: [m.sender], contextInfo: { externalAdReply: { title: "𖡛 LEVEL UP 𖡛", body: botname, thumbnail: imagen, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
-user.tawbot -= 300
+user.tawbot -= 100
 } else {
 let noFrag = `No tienes suficientes *[ 💠 Fragmentos ]* para subir de nivel.\n- Solo tienes 💠 *${user.tawbot} Fragmentos* en tu inventario.`
 imagen = Buffer.from(await (await fetch(`https://files.catbox.moe/6x77gd.jpg`)).arrayBuffer())
 return conn.sendMessage(m.chat, { text: noFrag, mentions: [m.sender], contextInfo: { externalAdReply: { title: "¡Insuficientes fragmentos!", body: botname, thumbnail: imagen, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
  }
 } else if (args[0] === "--rk") {
-if (user.puntos >= 350) {
+if (user.puntos >= 150) {
 estadistica = 1
 llaves = Math.floor(Math.random() * 2)
 monedas = Math.floor(Math.random() * 25)
@@ -64,10 +64,10 @@ let respRank = `· ┄ · ⊸ 𔓕 *New  :  Rank*
 ❒ *${currency}* : +${monedas.toLocaleString()}
 ❒ *${currency2}* : +${experiencia.toLocaleString()}
 
-> Consigue *[ 🌀 350 Puntos ]* para subir de rango y recibir recompensas.`
+> Consigue *[ 🌀 150 Puntos ]* para subir de rango y recibir recompensas.`
 imagen = Buffer.from(await (await fetch(`https://files.catbox.moe/10bj4k.jpg`)).arrayBuffer())
 await await conn.sendMessage(m.chat, { text: respRank, mentions: [m.sender], contextInfo: { externalAdReply: { title: "𖡛 NEW RANK 𖡛", body: botname, thumbnail: imagen, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
-user.puntos -= 350
+user.puntos -= 150
 } else {
 let noPunt = `No tienes suficientes *[ 🌀 Puntos ]* para subir de rango.\n- Solo tienes 🌀 *${user.tawbot} Puntos* en tu inventario.`
 imagen = Buffer.from(await (await fetch(`https://files.catbox.moe/2bndyf.jpg`)).arrayBuffer())
