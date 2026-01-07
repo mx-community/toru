1f7c68fbf81316d41ea00ebdb3e2eb0967b401dc
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-07 01:50:26 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -1,16 +1,34 @@
-import fetch from 'node-fetch'
-import axios from 'axios'
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-if (!text) return conn.sendMessage(m.chat, { text: `Escribe para crear una imagen.` }, { quoted: m })
-await m.react('⏳')
-const res = await global.sendOptishield({ type: "text2img", text: text})
-console.log(res)
-await conn.sendMessage(m.chat, { image: { url: res.result.img }, caption: "image-text2img.png" }, { quoted: m })
-await m.react('✅')
+  let user = global.db.data.users[m.sender]
+  let items = {
+    "boletos": "boletos",
+    "velas": "toruvela",
+    "piesas": "torupiesa"
+  }
+  let precios = {
+    "boletos": 10,
+    "velas": 10,
+    "piesas": 10
+  }
 
+  if (!text) return conn.sendMessage(m.chat, { text: "Por favor, ingresa el nombre del ítem y la cantidad" }, { quoted: m })
+
+  let [item, cantidad] = text.split(" ")
+  item = item.toLowerCase()
+
+  if (!items[item]) return conn.sendMessage(m.chat, { text: "Ítem no encontrado" }, { quoted: m })
+
+  cantidad = parseInt(cantidad)
+  if (isNaN(cantidad) || cantidad <= 0) return conn.sendMessage(m.chat, { text: "Cantidad inválida" }, { quoted: m })
+
+  let precioTotal = precios[item] * cantidad
+  if (user.coin < precioTotal) return conn.sendMessage(m.chat, { text: `No tienes suficientes fragmentos (${precioTotal} necesarios)` }, { quoted: m })
+
+  user.coin -= precioTotal
+  user[items[item]] += cantidad
+
+  conn.sendMessage(m.chat, { text: `Has comprado ${cantidad} ${item} por ${precioTotal} fragmentos` }, { quoted: m })
 }
 
-handler.command = ['xd']
-
+handler.command = ["shopp"]
 export default handler
-                                
