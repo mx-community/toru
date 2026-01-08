11491ef9a8c5a4d103a05e6ecb3cd8cb8c2adb37
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-08 10:18:37 -0300

    Update _test2.js

--- plugins/_test2.js
+++ plugins/_test2.js
@@ -1,39 +1,21 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
   let user = global.db.data.users[m.sender]
   let personajes = {
-    "tawc": { nombre: "Guts", rareza: "Común", valor: 1 },
-    "tawc2": { nombre: "Miyamoto", rareza: "Raro", valor: 3 },
-    "tawc3": { nombre: "Takeda", rareza: "Épico", valor: 5 }
+    "tawc": { nombre: "Guts", rareza: "Común", valor: 1988 },
+    "tawc2": { nombre: "Miyamoto", rareza: "Raro", valor: 3988 },
+    "tawc3": { nombre: "Takeda", rareza: "Épico", valor: 5988 }
   }
 
-  let personajesComprados = []
+  let mensaje = "Personajes comprados:\n"
+  let comprado = false
   for (let dbName in personajes) {
     if (user[dbName]) {
-      personajesComprados.push(personajes[dbName])
+      mensaje += `${personajes[dbName].nombre} = *(${personajes[dbName].rareza})*\n`
+      comprado = true
     }
   }
 
-  if (personajesComprados.length === 0) return conn.sendMessage(m.chat, { text: "No has comprado ningún personaje" }, { quoted: m })
-
-  let mensaje = "Personajes comprados:\n"
-  let epicos = personajesComprados.filter(p => p.rareza === "Épico")
-  let raros = personajesComprados.filter(p => p.rareza === "Raro")
-  let comunes = personajesComprados.filter(p => p.rareza === "Común")
-
-  if (epicos.length > 0) {
-    mensaje += "> Épico:\n"
-    epicos.forEach(p => mensaje += `  - ${p.nombre} (ID: ${Object.keys(personajes).find(key => personajes[key] === p)}) - Valor: ${p.valor}\n`)
-  }
-
-  if (raros.length > 0) {
-    mensaje += "> Raro:\n"
-    raros.forEach(p => mensaje += `  - ${p.nombre} (ID: ${Object.keys(personajes).find(key => personajes[key] === p)}) - Valor: ${p.valor}\n`)
-  }
-
-  if (comunes.length > 0) {
-    mensaje += "> Común:\n"
-    comunes.forEach(p => mensaje += `  - ${p.nombre} (ID: ${Object.keys(personajes).find(key => personajes[key] === p)}) - Valor: ${p.valor}\n`)
-  }
+  if (!comprado) mensaje = "No has comprado ningún personaje"
 
   conn.sendMessage(m.chat, { text: mensaje }, { quoted: m })
 }
