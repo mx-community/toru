7c1462a697d51cf3c7d084537bf2ea5a2f86a6c9
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-07 22:58:39 -0300

    Add files via upload

---
+++ plugins/rpg-stats.js
@@ -0,0 +1,53 @@
+let handler = async (m, {conn, usedPrefix, command}) => {
+if (!global.db.data.chats[m.chat].fRpg && m.isGroup) {
+return conn.sendMessage(m.chat, { text: `✦ Los comandos de *[ RPG ]* estan desactivados.\n- Un administrador puede activarlo con: *#fc-rpg on*` }, { quoted: m })
+}
+let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
+if (who == conn.user.jid) return error 
+if (!(who in global.db.data.users)) return conn.sendMessage(m.chat, { text: `El usuario mencionado no esta en la base de datos.` }, { quoted: m })
+let user = global.db.data.users[who]
+let imagen = Buffer.from(await (await fetch(`https://files.catbox.moe/smx37m.jpg`)).arrayBuffer())
+let yo = `· ┄ · ⊸ 𔓕 *STATS : RPG*
+- _Mira tus estadisticas en el modo rpg._
+
+> 🝐 *Proceso:*
+\t🜲 *Rango* : #${user.rangos}
+\t𖡛 *Nivel* : lvl_${user.nivele}
+
+> 🝐 *Estado:*
+\t❤️ *Salud* : ${user.health}%
+\t⛏️ *Pico* : ${user.torupico}%
+\t🗡️ *Espada* : ${user.toruesp}%
+\t🪓 *Hacha* : ${user.toruach}%
+
+> 📍  Puedes mejorar tus herramientas con los siguientes comandos.
+*#pico* --up
+*#espada* --up
+*#hacha* --up`
+let tu = `· ┄ · ⊸ 𔓕 *STATS : RPG*
+- _Mira las estadísticas de @${who.split`@`[0]}._
+
+> 🝐 *Proceso:*
+\t🜲 *Rango* : #${user.rangos}
+\t𖡛 *Nivel* : lvl_${user.nivele}
+
+> 🝐 *Estado:*
+\t❤️ *Salud* : ${user.health}%
+\t⛏️ *Pico* : ${user.torupico}%
+\t🗡️ *Espada* : ${user.toruesp}%
+\t🪓 *Hacha* : ${user.toruach}%
+
+> 📍  El usuario puede mejorar sus herramientas con los siguientes comandos.
+*#pico* --up
+*#espada* --up
+*#hacha* --up`
+await conn.sendMessage(m.chat, { text: `${who == m.sender ? `${yo}` : `${tu}`}`, mentions: [who], contextInfo: { externalAdReply: { title: "〩  S T A T S  〩", body: botname, thumbnail: imagen, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
+//conn.sendMessage(m.chat, { text: `${who == m.sender ? `${yo}` : `${tu}`}`, mentions: [who] }, { quoted: m }
+//m.reply(`${who == m.sender ? `${yo}` : `${tu}`}`, null, { mentions: [who] })
+}
+
+
+handler.command = ['stats', 'estadistica']
+handler.group = true 
+export default handler
+
---
+++ plugins/rpg-ups.js
@@ -0,0 +1,102 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, args, text, usedPrefix, command }) => {
+if (!global.db.data.chats[m.chat].fRpg && m.isGroup) {
+return conn.sendMessage(m.chat, { text: `✦ Los comandos de *[ RPG ]* estan desactivados.\n- Un administrador puede activarlo con: *#fc-rpg on*` }, { quoted: m })
+}
+
+let user = global.db.data.users[m.sender]
+let exito, noXd, imagen
+if (command === "pico") {
+if (!args[0]) {
+let pico = `· ┄ · ⊸ 𔓕 *Pico  :  RPG*
+- _Mejora tu pico de minería._
+
+> 〩 *Durabilidad:*
+\t⛏️ *${user.torupico}%*
+
+● Mejora  :  *$50 ${currency2}*
+● Utilidad  :  *#minar, #mining*
+● Consumo  :  *-10% por mina*
+
+> Usa *(${usedPrefix + command} --up)* para mejorar la durabilidad.`
+imagen = Buffer.from(await (await fetch(``)).arrayBuffer())
+await conn.sendMessage(m.chat, { text: pico, mentions: [m.sender], contextInfo: { externalAdReply: { title: "PICO  :  RPG", body: botname, thumbnail: imagen, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
+} else if (args[0] === "--up") {
+if (user.toruexp >= 50) {
+exito = `Mejoraste el *[ ⛏️ Pico ]* en +50 puntos de durabilidad.`
+return conn.sendMessage(m.chat, { text: exito }, { quoted: m })
+user.torupico += 50
+user.toruexp -= 50
+} else {
+imagen = Buffer.from(await (await fetch(`https://files.catbox.moe/r0t9ng.jpg`)).arrayBuffer())
+noXd = `No tienes suficientes *[ ✩ ${currency2} ]* para mejorar el pico.\n- Solo tienes ✩ *${user.toruexp} ${currency}* en tu inventario.`
+return conn.sendMessage(m.chat, { text: noXd, mentions: [m.sender], contextInfo: { externalAdReply: { title: "¡Sin estrellas suficientes!", body: botname, thumbnail: imagen, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
+}
+}
+}
+
+if (command === "espada") {
+if (!args[0]) {
+let espada = `· ┄ · ⊸ 𔓕 *Espada  :  RPG*
+- _Mejora tu pico de minería._
+
+> 〩 *Durabilidad:*
+\t🗡️ *${user.toruesp}%*
+
+● Mejora  :  *$50 ${currency2}*
+● Utilidad  :  *#hunt, #cazar*
+● Consumo  :  *-10% por cazar*
+
+> Usa *(${usedPrefix + command} --up)* para mejorar la durabilidad.`
+imagen = Buffer.from(await (await fetch(`https://files.catbox.moe/36pk4m.jpg`)).arrayBuffer())
+await conn.sendMessage(m.chat, { text: espada, mentions: [m.sender], contextInfo: { externalAdReply: { title: "PICO  :  RPG", body: botname, thumbnail: imagen, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
+} else if (args[0] === "--up") {
+if (user.toruexp >= 50) {
+exito = `Mejoraste la *[ 🗡️ Espada ]* en +50 puntos de durabilidad.`
+return conn.sendMessage(m.chat, { text: exito }, { quoted: m })
+user.toruesp += 50
+user.toruexp -= 50
+} else {
+imagen = Buffer.from(await (await fetch(`https://files.catbox.moe/r0t9ng.jpg`)).arrayBuffer())
+noXd = `No tienes suficientes *[ ✩ ${currency2} ]* para mejorar el pico.\n- Solo tienes ✩ *${user.toruexp} ${currency}* en tu inventario.`
+return conn.sendMessage(m.chat, { text: noXd, mentions: [m.sender], contextInfo: { externalAdReply: { title: "¡Sin estrellas suficientes!", body: botname, thumbnail: imagen, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
+}
+}
+}
+
+if (command === "hacha") {
+if (!args[0]) {
+let hacha = `· ┄ · ⊸ 𔓕 *Hacha  :  RPG*
+- _Mejora tu pico de minería._
+
+> 〩 *Durabilidad:*
+\t🪓 *${user.toruach}%*
+
+● Mejora  :  *$50 ${currency2}*
+● Utilidad  :  *#madera, #talar*
+● Consumo  :  *-10% por talar*
+
+> Usa *(${usedPrefix + command} --up)* para mejorar la durabilidad.`
+imagen = Buffer.from(await (await fetch(`https://files.catbox.moe/pg6w1t.jpg`)).arrayBuffer())
+await conn.sendMessage(m.chat, { text: hacha, mentions: [m.sender], contextInfo: { externalAdReply: { title: "PICO  :  RPG", body: botname, thumbnail: imagen, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
+} else if (args[0] === "--up") {
+if (user.toruexp >= 50) {
+exito = `Mejoraste el *[ 🪓 Hacha ]* en +50 puntos de durabilidad.`
+return conn.sendMessage(m.chat, { text: exito }, { quoted: m })
+user.toruach += 50
+user.toruexp -= 50
+} else {
+imagen = Buffer.from(await (await fetch(`https://files.catbox.moe/r0t9ng.jpg`)).arrayBuffer())
+noXd = `No tienes suficientes *[ ✩ ${currency2} ]* para mejorar el pico.\n- Solo tienes ✩ *${user.toruexp} ${currency}* en tu inventario.`
+return conn.sendMessage(m.chat, { text: noXd, mentions: [m.sender], contextInfo: { externalAdReply: { title: "¡Sin estrellas suficientes!", body: botname, thumbnail: imagen, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
+}
+}
+}
+
+}
+
+handler.command = ['pico', 'espada', 'hacha']
+handler.group = true
+
+export default handler
+
