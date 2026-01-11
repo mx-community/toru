0b05997c8e6544f05269e6cc4751c334cac039ca
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-11 19:07:52 -0300

    Update _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,19 +1,8 @@
-export axios from 'axios'
-let handler = async (m, { conn, usedPrefix, command }) => {
-if (!m.quoted) return conn.sendMessage(m.chat, { text: `ᗢ Responda a un video para convertirlo en gif.` }, { quoted: m })
-await react("⏰")
-try {
-const q = m.quoted || m
-let mime = (q.msg || q).mimetype || ''
-if (!/(mp4)/.test(mime)) return conn.sendMessage(m.chat, { text: `Solo puedes responder videos.` }, { quoted: m })
-let media = await q.download()
-const res = await global.sendOptishield({ type: media, text: null})
-conn.sendMessage(m.chat, { video: res.result.url, gifPlayback: true, caption: null }, { quoted: m })
-await m.react("✅")
-} catch (error) {
-await conn.sendMessage(m.chat, { text: `${error.message}` }, { quoted: m })
+let handler = async (m, { conn, usedPrefix, command, text, args }) => {
+let hola = `Hola...`
+let ch = `120363424098891946@newsletter`
+await conn.sendMessage(ch, { text: hola }, m)
 }
-}
-handler.command = ['vgif']
+handler.command = ["notch"]
 export default handler
-                 
+ 
