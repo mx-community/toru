9d39742c8a619943d8e86b7a68f393c69b598b1f
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-11 19:41:08 -0300

    Update _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,8 +1,14 @@
+import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command, text, args }) => {
-let hola = `Hola...`
-let ch = `120363424098891946@newsletter`
-await conn.sendMessage(ch, { text: hola }, m)
+if (!text) return conn.sendMessage(m.chat, { text: `Ingresa un texto para el canal.` }, { quoted: m })
+let imagen, ch
+let xd = `${text}`
+ch = `120363424098891946@newsletter`
+imagen = Buffer.from(await (await fetch(`https://files.catbox.moe/se54kc.jpg`)).arrayBuffer())
+await conn.sendMessage(ch, { text: xd, mentions: [m.sender], contextInfo: { externalAdReply: { title: "¡Hola!", body: botname, thumbnail: imagen, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, m)
+await conn.reply(m.chat, `Success`, m)
 }
 handler.command = ["notch"]
 export default handler
+ 
  
