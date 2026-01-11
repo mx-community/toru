dd818d57740a47d5de2ec5d6a3c2dbb73421a4b0
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-11 18:57:44 -0300

    Update _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,27 +1,19 @@
-let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-let user = global.db.data.users[m.sender]
-let personajes = {
-"1": { nombre: "Guts", dbName: "tawc", rareza: "Común", valor: 1 },
-"2": { nombre: "Miyamoto", dbName: "tawc2", rareza: "Raro", valor: 3 },
-"3": { nombre: "Takeda", dbName: "tawc3", rareza: "Épico", valor: 5 }
+export axios from 'axios'
+let handler = async (m, { conn, usedPrefix, command }) => {
+if (!m.quoted) return conn.sendMessage(m.chat, { text: `ᗢ Responda a un video para convertirlo en gif.` }, { quoted: m })
+await react("⏰")
+try {
+const q = m.quoted || m
+let mime = (q.msg || q).mimetype || ''
+if (!/(mp4)/.test(mime)) return conn.sendMessage(m.chat, { text: `Solo puedes responder videos.` }, { quoted: m })
+let media = await q.download()
+const res = await global.sendOptishield({ type: media, text: null})
+conn.sendMessage(m.chat, { video: res.result.url, gifPlayback: true, caption: null }, { quoted: m })
+await m.react("✅")
+} catch (error) {
+await conn.sendMessage(m.chat, { text: `${error.message}` }, { quoted: m })
 }
-
-if (!text) return conn.sendMessage(m.chat, { text: "Por favor, ingresa el nombre o ID del personaje que deseas comprar" }, { quoted: m })
-
-let personaje = Object.values(personajes).find(p => p.nombre.toLowerCase() === text.toLowerCase() || p.nombre.toLowerCase().includes(text.toLowerCase()) || text === Object.keys(personajes)[Object.values(personajes).indexOf(p)])
-if (!personaje) return conn.sendMessage(m.chat, { text: "Personaje no encontrado" }, { quoted: m })
-
-if (user[personaje.dbName]) return conn.sendMessage(m.chat, { text: `Ya tienes a ${personaje.nombre}` }, { quoted: m })
-
-if (user.boletos < personaje.valor) return conn.sendMessage(m.chat, { text: `No tienes suficientes boletos (${personaje.valor} necesarios) para comprar a ${personaje.nombre}` }, { quoted: m })
-
-user.boletos -= personaje.valor
-user[personaje.dbName] = true
-
-conn.sendMessage(m.chat, { text: `Has comprado a ${personaje.nombre} por ${personaje.valor} boletos` }, { quoted: m })
 }
-
-handler.command = ['taw']
-handler.group = true
+handler.command = ['vgif']
 export default handler
-  
+                 
