db840c0a78ec1830e654a5fcef0a2204483b5a7e
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-12 08:00:45 -0300

    Update own-wx.js

--- plugins/own-wx.js
+++ plugins/own-wx.js
@@ -6,7 +6,7 @@ if (!text) {
 const folders = ['plugins', 'database', 'lib', 'src'].filter(folder => 
 existsSync('./' + folder)
 )
-let message = `📍  *[ ARCHIVOS ]*\n\n`
+let message = `📍  *[ ARCHIVO ]*\n\n`
 folders.forEach(folder => {
 const files = readdirSync('./' + folder, { withFileTypes: true }).filter(file => file.isFile()).map(file => `⧡ ${file.name}`).join('\n')
 message += `*${folder}/:*\n${files || '```No files found.```'}\n\n`
@@ -21,7 +21,7 @@ return conn.sendMessage(m.chat, { text: `Not present = \`\`\`${fileParh}\`\`\`` 
 
 const content = readFileSync(filePath, 'utf8')
 const stats = existsSync(filePath)
-let contenido = `📍  *[ ARCHIVOS ]*\n- ${text}\n\n\`\`\`${content.substring(0, 1000)}${content.length > 1000 ? '...' : ''}\`\`\``
+let contenido = `📍  *[ ARCHIVO ]*\n- ${text}\n\n\`\`\`${content.substring(0, 9000)}\`\`\``
 await conn.sendMessage(m.chat, { text: contenido }, { quoted: m })
 } catch (error) {
 await conn.sendMessage(m.chat, { text: `${error.message}` }, { quoted: m })
@@ -32,3 +32,4 @@ await conn.sendMessage(m.chat, { text: `${error.message}` }, { quoted: m })
 handler.command = ['wx-']
 handler.rowner = true
 export default handler
+//${content.length > 1000 ? '...' : ''}
