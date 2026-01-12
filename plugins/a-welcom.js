c92d10c3a3c5d0b17ef0bec07c0948332eaf0d8e
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-12 11:36:39 -0300

    Add files via upload

--- plugins/a-welcom.js
+++ plugins/a-welcom.js
@@ -1,139 +1,58 @@
 import fs from 'fs'
-import fetch from 'node-fetch'
-import FormData from 'form-data'
-import axios from 'axios'
-import { WAMessageStubType, generateWAMessageContent, generateWAMessageFromContent, proto } from '@whiskeysockets/baileys'
-
-function detectarPais(numero) {
-const codigos = {
-"1": "🇺🇸 EE.UU / 🇨🇦 Canadá", "7": "🇷🇺 Rusia / 🇰🇿 Kazajistán",
-"20": "🇪🇬 Egipto", "27": "🇿🇦 Sudáfrica", "30": "🇬🇷 Grecia",
-"31": "🇳🇱 Países Bajos", "32": "🇧🇪 Bélgica", "33": "🇫🇷 Francia",
-"34": "🇪🇸 España", "36": "🇭🇺 Hungría", "39": "🇮🇹 Italia",
-"40": "🇷🇴 Rumania", "44": "🇬🇧 Reino Unido", "49": "🇩🇪 Alemania",
-"51": "🇵🇪 Perú", "52": "🇲🇽 México", "53": "🇨🇺 Cuba",
-"54": "🇦🇷 Argentina", "55": "🇧🇷 Brasil", "56": "🇨🇱 Chile",
-"57": "🇨🇴 Colombia", "58": "🇻🇪 Venezuela", "591": "🇧🇴 Bolivia",
-"593": "🇪🇨 Ecuador", "595": "🇵🇾 Paraguay", "598": "🇺🇾 Uruguay",
-"502": "🇬🇹 Guatemala", "503": "🇸🇻 El Salvador",
-"504": "🇭🇳 Honduras", "505": "🇳🇮 Nicaragua",
-"506": "🇨🇷 Costa Rica", "507": "🇵🇦 Panamá",
-"60": "🇲🇾 Malasia", "61": "🇦🇺 Australia", "62": "🇮🇩 Indonesia",
-"63": "🇵🇭 Filipinas", "64": "🇳🇿 Nueva Zelanda",
-"65": "🇸🇬 Singapur", "66": "🇹🇭 Tailandia",
-"81": "🇯🇵 Japón", "82": "🇰🇷 Corea del Sur", "84": "🇻🇳 Vietnam",
-"86": "🇨🇳 China", "90": "🇹🇷 Turquía", "91": "🇮🇳 India",
-"212": "🇲🇦 Marruecos", "213": "🇩🇿 Argelia",
-"216": "🇹🇳 Túnez", "218": "🇱🇾 Libia",
-"234": "🇳🇬 Nigeria", "254": "🇰🇪 Kenia",
-"255": "🇹🇿 Tanzania", "256": "🇺🇬 Uganda",
-"258": "🇲🇿 Mozambique", "260": "🇿🇲 Zambia",
-"263": "🇿🇼 Zimbabue"
-}
-for (const code in codigos)
-if (numero.startsWith(code)) return codigos[code]
-return "Desconocido"
-}
-
-let thumb = await fetch(`${global.toruImg}`)
-.then(res => res.arrayBuffer()).catch(() => null)
-
-const fkontak = {
-key: { participant: '0@s.whatsapp.net', remoteJid: 'status@broadcast', id: 'Halo' },
-message: { locationMessage: { name: botname, jpegThumbnail: Buffer.from(thumb || []) } }
-}
-
-function fechaHoraArg() {
-const fecha = new Date().toLocaleDateString("es-AR", {
-weekday: 'long', day: 'numeric', month: 'long', year: 'numeric',
-timeZone: "America/Buenos_Aires"
-})
-const hora = new Date().toLocaleTimeString("es-AR", {
-hour: "2-digit", minute: "2-digit", second: "2-digit",
-timeZone: "America/Buenos_Aires"
-})
-return { fecha, hora }
-}
-
-
+import { WAMessageStubType } from '@whiskeysockets/baileys'
 
 async function generarBienvenida({ conn, userId, groupMetadata, chat }) {
 const username = `@${userId.split('@')[0]}`
-const numero = userId.split("@")[0]
-const pais = detectarPais(numero)
-
 const pp = await conn.profilePictureUrl(userId, 'image').catch(() => `${global.toruImg}`)
-
-const { fecha, hora } = fechaHoraArg()
+const fecha = new Date().toLocaleDateString("es-ES", { timeZone: "America/Buenos_Aires", day: 'numeric', month: 'long', year: 'numeric' })
 const groupSize = groupMetadata.participants.length + 1
 const desc = groupMetadata.desc?.toString() || 'Sin descripción'
+const mensaje = (chat.sWelcome || 'Puedes editar con el comando *#welcome+*').replace(/{usuario}/g, `${username}`).replace(/{grupo}/g, `*${groupMetadata.subject}*`).replace(/{desc}/g, `${desc}`)
 
-const mensaje = (chat.sWelcome || 'Edita con el comando "setwelcome"')
-.replace(/{usuario}/g, `${username}`)
-.replace(/{grupo}/g, `${groupMetadata.subject}`)
-.replace(/{desc}/g, `${desc}`)
+const caption = `👋🏻  @${username}
 
-let toruWelcome = `Hola chatgpt, ahora seras [ @TORU ], en esta pequeña petición quiero que des la bienvenida al nuevo usuario que ingreso a este grupo, solamente genera el texto corto de bienvenida no digas otra cosa mas como "Esta Bien", "¡Claro! Aqui esta el texto de bienvenida", nada de eso, solamente manda la bienvenida, respetar al usuario y que esperamos este bien y se sienta cómodo.`
-let { data } = await axios.get(`https://api-hasumi.vercel.app/api/ai/chatgpt?text=${encodeURIComponent(toruWelcome)}`)
-const bienvenidaXd = `👋🏻  @${username}\n\n${data.texto}`
-})
+_Bienvenido/a al grupo, espero que estes cómodo o cómoda en este chat, puedes usar *( #menu )* para ver la lista de categorías._
 
-return { pp, bienvenidaXd, mentions: [userId] }
+> ${textbot}`
+
+return { pp, caption, mentions: [userId] }
 }
-
 
 async function generarDespedida({ conn, userId, groupMetadata, chat }) {
 const username = `@${userId.split('@')[0]}`
-const numero = userId.split("@")[0]
-const pais = detectarPais(numero)
-
 const pp = await conn.profilePictureUrl(userId, 'image').catch(() => `${global.toruImg}`)
-
-const { fecha, hora } = fechaHoraArg()
+const fecha = new Date().toLocaleDateString("es-ES", { timeZone: "America/Buenos_Aires", day: 'numeric', month: 'long', year: 'numeric' })
 const groupSize = groupMetadata.participants.length - 1
 const desc = groupMetadata.desc?.toString() || 'Sin descripción'
+const mensaje = (chat.sBye || 'Puedes editar con el comando *#bye+*').replace(/{usuario}/g, `${username}`).replace(/{grupo}/g, `${groupMetadata.subject}`).replace(/{desc}/g, `*${desc}*`)
 
-const mensaje = (chat.sBye || 'Edita con el comando "setbye"')
-.replace(/{usuario}/g, `${username}`)
-.replace(/{grupo}/g, `${groupMetadata.subject}`)
-.replace(/{desc}/g, `*${desc}*`)
+const caption = `👋🏻  @${username}
 
-let toruDespedida = `Hola chatgpt, ahora seras [ @TORU ], en esta pequeña petición quiero que des la despedida a un usuario que se retiro de este grupo, solamente genera el texto corto de despedida no digas otra cosa mas como "Esta Bien", "¡Claro! Aqui esta el texto de despedida", nada de eso, solamente manda la despedida, desearle suerte al usuario y que esperamos este bien y se sienta cómodo.`
-let { data } = await axios.get(`https://api-hasumi.vercel.app/api/ai/chatgpt?text=${encodeURIComponent(toruDespedida)}`)
-const despedidaXd = `👋🏻  ${username}\n\n${data.texto}`
-return { pp, despedidaXd, mentions: [userId] }
+_Damos la despedida a un miembro del grupo, se retiro del chat, esperamos y se encuentre bien._
+
+> ${textbot}`
+
+return { pp, caption, mentions: [userId] }
 }
 
 let handler = m => m
-
 handler.before = async function (m, { conn, participants, groupMetadata }) {
-try {
 if (!m.messageStubType || !m.isGroup) return !0
-
+const primaryBot = global.db.data.chats[m.chat].primaryBot
+if (primaryBot && conn.user.jid !== primaryBot) throw !1
 const chat = global.db.data.chats[m.chat]
 const userId = m.messageStubParameters[0]
 
 if (chat.welcome && m.messageStubType == WAMessageStubType.GROUP_PARTICIPANT_ADD) {
-const { pp, bienvenidaXd, mentions } = await generarBienvenida({ conn, userId, groupMetadata, chat })
-const thumbWel = Buffer.from(await (await fetch(`${global.toruImg}`)).arrayBuffer())
-await conn.sendMessage(m.chat, { text: bienvenidaXd, mentionedJid: mentions, contextInfo: { externalAdReply: { title: "〩 ¡Bienvenido!", body: botname, thumbnail: thumbWel, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: fkontak })
+const { pp, caption, mentions } = await generarBienvenida({ conn, userId, groupMetadata, chat })
+await conn.sendMessage(m.chat, { image: { url: pp }, caption, mentionedJid: mentions }, { quoted: null })
+try { fs.unlinkSync(img) } catch {}
 }
 
-if (chat.welcome &&
-(m.messageStubType == WAMessageStubType.GROUP_PARTICIPANT_REMOVE ||
- m.messageStubType == WAMessageStubType.GROUP_PARTICIPANT_LEAVE)) {
-
+if (chat.welcome && (m.messageStubType == WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType == WAMessageStubType.GROUP_PARTICIPANT_LEAVE)) {
 const { pp, caption, mentions } = await generarDespedida({ conn, userId, groupMetadata, chat })
-const thumbBye = Buffer.from(await (await fetch(`${global.toruImg}`)).arrayBuffer())
-await conn.sendMessage(m.chat, { text: despedidaXd, mentionedJid: mentions, contextInfo: { externalAdReply: { title: "〩 ¡Despedida!", body: botname, thumbnail: thumbBye, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: fkontak })
-}
-
-} catch (e) {
-console.error(e)
-await conn.sendMessage(m.chat, {
-text: `${e.message}`,
-mentions: [m.sender]
-})
+await conn.sendMessage(m.chat, { image: { url: pp }, caption, mentionedJid: mentions }, { quoted: null })
+try { fs.unlinkSync(img) } catch {}
 }
 }
 
