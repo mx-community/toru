9deea72082aa1b15c638329e660dc6f23940fcd0
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-15 14:44:09 -0300

    Add files via upload

--- plugins/s-imagen.js
+++ plugins/s-imagen.js
@@ -1,5 +1,35 @@
 import axios from 'axios'
+import baileys from '@whiskeysockets/baileys'
 const handler = async (m, { conn, text, usedPrefix, command }) => {
+
+async function sendAlbumMessage(jid, medias, options = {}) {
+if (typeof jid !== "string") { throw new TypeError(`jid must be string, received: ${jid}`) }
+if (!Array.isArray(medias) || medias.length < 2) { throw new RangeError("Minimum 2 media required") }
+for (const media of medias) {
+if (!media.type || (media.type !== "image" && media.type !== "video")) { throw new TypeError(`Invalid media type: ${media.type}`) }
+if (!media.data || (!media.data.url && !Buffer.isBuffer(media.data))) { throw new TypeError(`Invalid media data`) }}
+const caption = options.text || options.caption || ""
+const delay = !isNaN(options.delay) ? options.delay : 500
+delete options.text
+delete options.caption
+delete options.delay
+const album = baileys.generateWAMessageFromContent(jid, {
+messageContextInfo: {},
+albumMessage: {
+expectedImageCount: medias.filter(m => m.type === "image").length,
+expectedVideoCount: medias.filter(m => m.type === "video").length,
+...(options.quoted ? { contextInfo: { remoteJid: options.quoted.key.remoteJid, fromMe: options.quoted.key.fromMe, stanzaId: options.quoted.key.id, participant: options.quoted.key.participant || options.quoted.key.remoteJid, quotedMessage: options.quoted.message }} : {}),
+}}, {})
+await conn.relayMessage(album.key.remoteJid, album.message, { messageId: album.key.id })
+for (let i = 0; i < medias.length; i++) {
+const { type, data } = medias[i]
+const mediaMsg = await baileys.generateWAMessage(album.key.remoteJid, { [type]: data, ...(i === 0 ? { caption } : {}) }, { upload: conn.waUploadToServer })
+mediaMsg.message.messageContextInfo = { messageAssociation: { associationType: 1, parentMessageKey: album.key }, }
+await conn.relayMessage(mediaMsg.key.remoteJid, mediaMsg.message, { messageId: mediaMsg.key.id })
+await baileys.delay(delay) }
+return album
+}
+
 if (!text) return conn.sendMessage(m.chat, { text: `ᗢ Proporcione un texto para buscar imágenes.\n\n\t⚶ Por ejemplo:\n*${usedPrefix + command}* Arboles` }, { quoted: m })
 try {
 await m.react('⏰')
