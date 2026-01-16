280241a35a88aaccdde22721db83bee90e364350
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-16 02:46:06 -0300

    Update rpg-coins.js

--- plugins/rpg-coins.js
+++ plugins/rpg-coins.js
@@ -5,8 +5,8 @@ const handler = async (m, { conn, usedPrefix, command }) => {
 let user = global.db.data.users[m.sender]
 let mensaje = `· ┄ · ⊸ 𔓕 *Coins  :  Stat*
 
-\t💵 *${currency}* : ${user.torucoin}
-\t🪙 *${currency2}* : ${user.toruexp}
+\t💵 *${currency}* : ${toNum(user.torucoin)} *(${user.torucoin})*
+\t🪙 *${currency2}* : ${toNum(user.toruexp)} *(${user.toruexp})*
 
 > ${textbot}`
 conn.sendMessage(m.chat, { text: mensaje }, { quoted: m })
@@ -15,3 +15,7 @@ conn.sendMessage(m.chat, { text: mensaje }, { quoted: m })
 handler.command = ['coins']
 handler.group = true
 export default handler
+
+function toNum(number) {
+if (number >= 1000 && number < 1000000) { return (number / 1000).toFixed(1) + 'k' } else if (number >= 1000000) { return (number / 1000000).toFixed(1) + 'M' } else if (number <= -1000 && number > -1000000) { return (number / 1000).toFixed(1) + 'k' } else if (number <= -1000000) { return (number / 1000000).toFixed(1) + 'M' } else { return number.toString() }}
+                                                  
