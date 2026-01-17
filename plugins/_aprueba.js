e4da6e817d6be8701a7259f3a236ad0f364411cc
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-17 13:37:28 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -1,49 +1,13 @@
-import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
-import * as fs from 'fs'
+const handler = async (m, { conn }) => {
+if ("status@broadcast" != m.quoted?.chat) return conn.reply(m.chat, "*Por favor, responde a un estado de WhatsApp para descargar su contenido*", m) 
 
-let handler = async (m, { conn, text, usedPrefix }) => {
-if (!m.quoted && !text) return conn.reply(m.chat, `❀ Por favor, escribe el texto que deseas repetir.`, m)
-let mentionedJid = await m.mentionedJid
-let htextos = text ? text : (m.quoted && m.quoted.text) ? m.quoted.text : "¡¡¡Hola!!!"
-if ((mentionedJid && mentionedJid.length) || (m.quoted && m.quoted.mentionedJid && m.quoted.mentionedJid.length)) {
-let copy = htextos
-let list = mentionedJid || m.quoted.mentionedJid
-for (let i = 0; i < list.length; i++) {
-let num = list[i].split('@')[0]
-copy = copy.replace(/@\S+/, '@' + num)
-}
-htextos = copy
-}
-let users = mentionedJid?.length ? mentionedJid : m.quoted?.mentionedJid?.length ? m.quoted.mentionedJid : []
-let mentions = users.map(jid => conn.decodeJid(jid))
 try {
-let q = m.quoted ? m.quoted : m || m.text || m.sender
-let c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender
-let msg = generateWAMessageFromContent(m.chat, { extendedTextMessage: { text: htextos, contextInfo: { mentionedJid: mentions }}}, { quoted: null, userJid: conn.user.id })
-await conn.relayMessage(m.chat, msg.message, { messageId: msg.key?.id || undefined })
-} catch {
-let quoted = m.quoted ? m.quoted : m
-let mime = (quoted.msg || quoted).mimetype || ''
-let isMedia = /image|video|sticker|audio/.test(mime)
-if ((isMedia && quoted.mtype === 'imageMessage') && htextos) {
-var mediax = await quoted.download?.()
-conn.sendMessage(m.chat, { image: mediax, caption: htextos, mentions }, { quoted: null })
-} else if ((isMedia && quoted.mtype === 'videoMessage') && htextos) {
-var mediax = await quoted.download?.()
-conn.sendMessage(m.chat, { video: mediax, mimetype: 'video/mp4', caption: htextos, mentions }, { quoted: null })
-} else if ((isMedia && quoted.mtype === 'audioMessage') && htextos) {
-var mediax = await quoted.download?.()
-conn.sendMessage(m.chat, { audio: mediax, mimetype: 'audio/mp4', fileName: `audio.mp4`, mentions }, { quoted: null })
-} else if ((isMedia && quoted.mtype === 'stickerMessage') && htextos) {
-var mediax = await quoted.download?.()
-conn.sendMessage(m.chat, { sticker: mediax, mentions }, { quoted: null })
-} else {
-await conn.relayMessage(m.chat, { extendedTextMessage: { text: `${htextos}\n`, contextInfo: { mentionedJid: mentions }}})
-}}}
+let buffer = await m.quoted?.download()
+await conn.sendFile(m.chat, buffer, "", m.quoted?.text || "", null, false, { quoted: m })
+} catch (e) {
+console.log(e)
+await conn.reply(m.chat, m.quoted?.text, m)
+}}
 
-handler.help = ['say']
-handler.command = ['say', 'decir']
-handler.tags = ['tools']
-handler.group = true
-
+handler.command = ["story"]
 export default handler
