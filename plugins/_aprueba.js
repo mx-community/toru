d3f77840ac74a6ae94bbb0d3ad189d95478d24e9
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-17 03:12:51 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -1,1 +1,23 @@
+import fetch from 'node-fetch'
+const handler = async (m, { conn, command, args, usedPrefix, text }) => {
+const user = global.db.data.users[m.sender] || {};
+const name = await conn.getName(m.sender);
+const imwel = Buffer.from(await (await fetch(`https://files.catbox.moe/hnn3hp.jpg`)).arrayBuffer())
+const imbye = Buffer.from(await (await fetch(`https://files.catbox.moe/tln7ks.jpg`)).arrayBuffer())
+let hola = `Hola`
+await conn.sendMessage(m.chat, { text: hola, 
+contextInfo: { forwardingScore: 1, 
+isForwarded: false, 
+externalAdReply: { 
+showAdAttribution: true, 
+renderLargerThumbnail: true, 
+title: botname, 
+body: textbot, 
+containsAutoReply: true, 
+mediaType: 1, 
+thumbnailUrl: imwel, 
+sourceUrl: null }}}, { quoted: fkontak })
+};
+handler.command = ["dat"]
 
+export default handler;
