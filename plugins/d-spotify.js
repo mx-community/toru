56aea42f3a3a9167fd00596e0140434a4d6e9924
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-17 00:41:12 -0300

    Update d-spotify.js

--- plugins/d-spotify.js
+++ plugins/d-spotify.js
@@ -1,61 +1,44 @@
-/*import axios from 'axios';
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.sendMessage(m.chat, { text: `ᗢ Proporcione un texto o un enlace de *Spotify* para descargarlo.\n\n\t⚶ Por ejemplo:\n*${usedPrefix + command}* Golden Brown` }, { quoted: m });
-await m.react("⏰");
+import fetch from 'node-fetch'
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) return conn.sendMessage(m.chat, { text: `ᗢ Proporcione un enlace de Spotify.\n\n\t⚶ Por ejemplo:\n*${usedPrefix + command}* https://open.spotify.com/xxx` }, { quoted: m })
 try {
-let song;
-const isSpotifyUrl = text.startsWith('https://open.spotify.com/');
-if (isSpotifyUrl) {
-song = { url: text };
-} else {
-const results = await spotifyxv(text);
-if (!results.length) return conn.sendMessage(m.chat, { text: `No se han encontrado resultados sobre esa canción.` }, { quoted: m });
-song = results[0];
-}
+await m.react("⏰")
+const res = await fetch(`https://api.delirius.store/download/spotifydl?url=${text}`)
+const json = await res.json()
+const toru = json.data
+if (!/^(https?:\/\/)?(www\.)?(open\.spotify\.com)\//i.test(text)) return conn.sendMessage(m.chat, { text: `El enlace ingresado no es valido.` }, { quoted: m })
 
-const res = await axios.get(`https://api.stellarwa.xyz/dow/spotify?url=${song.url}&apikey=proyectsV2`);
-const data = res.data?.data;
-if (!data?.download) return conn.sendMessage(m.chat, { text: `No se ha podido acceder al enlace.` }, { quoted: m });
-const info = `· ┄ · ⊸ 𔓕 *Spotify  :  Download*
+let mensaje = `· ┄ · ⊸ 𔓕 *Spotify  :  Download*
 
-\t＃ *Titulo* : ${data.title}
-\t＃ *Artista* : ${data.artist} ${song.album ? `\n\t＃ *Album* : ${song.album}` : ''}
-\t＃ *Duracion* : ${data.duration}
-\t＃ *Enlace* : ${song.url}
+\t＃ *Titulo* : ${toru.title}
+\t＃ *Autor/a* : ${toru.author}
+\t＃ *Duracion* : ${secondString(toru.duration)}
 
 > ${textbot}`
-const thumb = (await conn.getFile(data.image))?.data
-await conn.sendMessage(m.chat, { text: info, mentions: [m.sender], contextInfo: { externalAdReply: { title: data.title, body: botname, thumbnail: thumb, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
-                       
-//await conn.sendMessage(m.chat, { image: { url: data.image }, caption: info }, { quoted: m });
-
-await conn.sendMessage(m.chat, {
-audio: { url: data.download },
-fileName: `${data.title}.mp3`,
-mimetype: 'audio/mpeg'
-}, { quoted: m });
-
+const thumb = (await conn.getFile(toru.image))?.data
+await conn.sendMessage(m.chat, { text: mensaje, mentions: [m.sender], contextInfo: { externalAdReply: { title: "⧿ Spotify : Download ⧿", body: botname, thumbnail: thumb, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
+await conn.sendMessage(m.chat, { audio: { url: toru.download }, mimetype: "audio/mpeg", fileName: toru.title }, { quoted: m })
+await m.react("✅")
 } catch (e) {
-await conn.sendMessage(m.chat, { text: `${e.message}` }, { quoted: m });
-}
-};
-handler.command = ['spotify', 'spy'];
-export default handler;
+await conn.sendMessage(m.chat, { text: `${e.message}` }, { quoted: m })
+}}
 
-async function spotifyxv(query) {
-const res = await axios.get(`https://api.stellarwa.xyz/search/spotify?query=${encodeURIComponent(query)}&apikey=proyectsV2`);
-if (!res.data?.status || !res.data?.data?.length) return [];
+handler.command = ['spy', 'spotify']
+export default handler
 
-const firstTrack = res.data.data[0];
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
 
-return [{
-name: firstTrack.title,
-artista: [firstTrack.artist],
-album: firstTrack.album,
-duracion: firstTrack.duration,
-url: firstTrack.url,
-imagen: firstTrack.image || ''
-}];
-}
-  
-*/
+function secondString(seconds) {
+seconds = Number(seconds)
+const d = Math.floor(seconds / (3600 * 24))
+const h = Math.floor((seconds % (3600 * 24)) / 3600)
+const m = Math.floor((seconds % 3600) / 60)
+const s = Math.floor(seconds % 60)
+const dDisplay = d > 0 ? d + (d == 1 ? 'd:' : 'd:') : ''
+const hDisplay = h > 0 ? h + (h == 1 ? 'h:' : 'h:') : ''
+const mDisplay = m > 0 ? m + (m == 1 ? 'm:' : 'm:') : ''
+const sDisplay = s > 0 ? s + (s == 1 ? 's' : 's') : ''
+return dDisplay + hDisplay + mDisplay + sDisplay
+  }
+                                                           
