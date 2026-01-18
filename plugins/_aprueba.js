cd2c910cc28ce9551d6ddcbeaf64f38841fc6db5
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-18 17:04:39 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -1,1 +1,16 @@
+import axios from 'axios'
+import fetch from 'node-fetch'
+let handler = async (m, { conn }) => {
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let who2 = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false 
+let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://i.imgur.com/whjlJSf.jpg')
+let me = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.imgur.com/whjlJSf.jpg')
+let name = conn.getName(who)
+let api = "https://api.delirius.store/canvas"
+await conn.sendMessage(m.chat, { image: { url: api + "/gay?url=" + pp }, caption: "Gay" }, { quoted: m })
+//conn.sendFile(m.chat, global.toru(nami, '/gay?url=', { avatar: pp }), 'toru.png', `🏳️‍🌈 Gay Card @${name}`, m)
+}
 
+handler.command = ['gay'] 
+export default handler
+  
