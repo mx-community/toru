a98948597ddec5e6265fac2981ce0af826123ece
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-19 12:00:07 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -1,12 +1,28 @@
-import axios from 'axios'
 import fetch from 'node-fetch'
-let handler = async (m, {conn, text, usedPrefix, command}) => {
-let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
-if (!teks) return conn.reply(m.chat, `escribe`, m )
-let img = 'https://api.delirius.store' + '/canvas/book?text' + teks
-conn.sendFile(m.chat, img, 'toru.png', `Ya`, m)
-}
+const handler = async (m, { conn, text, args, usedPrefix, command }) => {
+if (!text) return conn.sendMessage(m.chat, { text: `ᗢ Proporcione un enlace de Spotify.\n\n\t⚶ Por ejemplo:\n*${usedPrefix + command}* https://open.spotify.com/xxx` }, { quoted: m })
+try {
+await m.react("⏰")
+const res = await fetch(`https://api-hasumi.vercel.app/api/youtube/ytmp4v3?url=${text}`)
+const json = await res.json()
+const toru = json.data
+if (!/^(https?:\/\/)?(www\.)?(youtube\.com|youtu.be\.com)\//i.test(text)) return conn.sendMessage(m.chat, { text: `El enlace ingresado no es valido.` }, { quoted: m })
 
-handler.command = ['txt']
+let mensaje = `· ┄ · ⊸ 𔓕 *YouTube  :  MP4*
+
+\t＃ *Titulo* : ${toru.title}
+
+> ${textbot}`
+const thumb = Buffer.from(await (await fetch(`https://files.catbox.moe/6lknyb.jpg`)).arrayBuffer())
+//(await conn.getFile(toru.image))?.data
+await conn.sendMessage(m.chat, { text: mensaje, mentions: [m.sender], contextInfo: { externalAdReply: { title: "⧿ YouTube : Download ⧿", body: botname, thumbnail: thumb, sourceUrl: null, mediaType: 1, renderLargerThumbnail: false }}}, { quoted: m })
+await conn.sendMessage(m.chat, { audio: { url: toru.dl_url }, mimetype: "audio/mpeg", fileName: toru.title }, { quoted: m })
+await m.react("✅")
+} catch (e) {
+await conn.sendMessage(m.chat, { text: `${e.message}` }, { quoted: m })
+}}
+
+handler.command = ['pru']
 export default handler
 
+  
