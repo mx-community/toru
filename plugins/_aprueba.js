99515b3072dbbcd5fdacdf508f0ffb8d0369a4af
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-21 02:42:31 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -1,1 +1,34 @@
 
+import fetch from 'node-fetch'
+
+let handler = async (m, { text, usedPrefix, command }) => {
+
+if (!text) return conn.reply(m.chat, `✦ Escribe el nonbre del scraper.\nEjemplo: ${usedPrefix + command} yt-search`, m)
+
+try {
+
+let res = await fetch(`http://registry.npmjs.com/-/v1/search?text=${text}`)
+let { objects } = await res.json()
+
+if (!objects.length) return conn.reply(m.chat, `『✦』 No se encontró resultado de: ${text}`, m)
+
+let txt = objects.map(({ package: pkg }) => {
+return `《 ✧ 》 𝖲craper - ${botname} 《 ✧ 》
+
+✦ 𝐍𝐨𝐦𝐛𝐫𝐞: ${pkg.name}
+✦ 𝐕𝐞𝐫𝐬𝐢𝐨𝐧: V${pkg.version}
+✦ 𝐄𝐧𝐥𝐚𝐜𝐞: ${pkg.links.npm}
+✦ 𝐃𝐞𝐬𝐜𝐫𝐢𝐩𝐜𝐢𝐨𝐧: ${pkg.description}
+\n\n----------`
+}).join`\n\n`
+
+await conn.reply(m.chat, txt, m)
+} catch {
+await conn.reply(m.chat, 'Ocurrió un error', m)
+}}
+
+handler.help = ['npmjs']
+handler.tags = ['buscador']
+handler.command = ['npmjs']
+export default handler
+  
