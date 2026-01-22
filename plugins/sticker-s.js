ee0b1f4ee3126c1d2366ea23f5fe596b6f530601
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-22 01:34:22 -0300

    Update sticker-s.js

--- plugins/sticker-s.js
+++ plugins/sticker-s.js
@@ -1,25 +1,30 @@
 import { sticker } from '../lib/sticker.js'
 
 let handler = async (m, { conn }) => {
-if (!global.db.data.chats[m.chat].fStickers && m.isGroup) {
-return conn.sendMessage(m.chat, { text: `📍  Los comandos de *[ stickers ]* estan desactivados...` }, { quoted: m })
-}
+  try {
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || q.mediaType || ''
 
-try {
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || q.mediaType || ''
-if (!/image|video|webp/.test(mime))
-return conn.sendMessage(m.chat, { text: `ᗢ Responda a una imagen o video para crear un sticker.` }, { quoted: m })
+    if (!/image|video|webp/.test(mime))
+      return m.reply('Responde a una imagen, video o sticker')
 
-if (/video/.test(mime)) {
-let dur = (q.msg || q).seconds || 0
-if (dur > 7) return conn.sendMessage(m.chat, { text: `El video no debe durar mas de 7 segundos...` }, { quoted: m })
-}
+    if (/video/.test(mime)) {
+      let dur = (q.msg || q).seconds || 0
+      if (dur > 7) return m.reply('Duración máxima 7 segundos')
+    }
 
-await conn.sendMessage(m.chat, { react: { text: '⏰', key: m.key }})
-let media = await q.download()
-if (!media) return conn.sendMessage(m.chat, { text: 'No se pudo obtener el archivo' }, { quoted: m })
-let stiker = await sticker(media, global.packsticker || '', global.packsticker2 || '' )
+    await conn.sendMessage(m.chat, {
+      react: { text: '🕒', key: m.key }
+    })
+
+    let media = await q.download()
+    if (!media) throw 'No se pudo obtener el archivo'
+
+    let stiker = await sticker(
+      media,
+      global.packsticker || '',
+      global.packsticker2 || ''
+    )
 
     await conn.sendMessage(
       m.chat,
@@ -32,11 +37,15 @@ let stiker = await sticker(media, global.packsticker || '', global.packsticker2 
     })
 
   } catch (e) {
-    conn.sendMessage(m.chat, { text: `${e.message}` }, { quoted: m })
-    //m.reply('Error al crear el sticker')
+    await conn.sendMessage(m.chat, {
+      react: { text: '❌', key: m.key }
+    })
+    m.reply('Error al crear el sticker')
   }
 }
 
+handler.help = ['s']
+handler.tags = ['tools']
 handler.command = ['s', 'sticker']
 
 export default handler
