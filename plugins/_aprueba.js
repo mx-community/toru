d85e4a0e00c5c34a4e1e40e0b82bc42bf25e3184
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-22 21:52:36 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -3,10 +3,10 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 try {
 let respuesta = `Hola Hola Hola Hola.`
-const thumb = Buffer.from(await (await fetch(`${global.toruImg}`)).arrayBuffer())
+const thumb = Buffer.from(await (await fetch(`${toruMenu}`)).arrayBuffer())
 await conn.sendMessage(m.chat, { text: respuesta, 
 contextInfo: { forwardingScore: 1, isForwarded: false, 
-externalAdReply: { showAdAttribution: false, renderLargerThumbnail: true, title: "Prueba", body: botname, containsAutoReply: true, mediaType: 1, thumbnailUrl: "https://files.catbox.moe/ber8mc.jpg", sourceUrl: null }}}, { quoted: m })
+externalAdReply: { showAdAttribution: false, renderLargerThumbnail: true, title: "Prueba", body: botname, containsAutoReply: true, mediaType: 1, thumbnailUrl: thumb || "https://files.catbox.moe/ber8mc.jpg", sourceUrl: null }}}, { quoted: m })
 await m.react("✅")
 } catch (error) {
 conn.sendMessage(m.chat, { text: `${error.message}` }, { quoted: m })
