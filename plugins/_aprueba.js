78195bb1d825a6e447ebf3730534172e384d6763
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-22 21:48:41 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -1,15 +1,19 @@
+import axios from 'axios'
 import fetch from 'node-fetch' 
-let handler = async (m, { conn, command, usedPrefix }) => {
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+try {
+let respuesta = `Hola Hola Hola Hola.`
 const thumb = Buffer.from(await (await fetch(`${global.toruImg}`)).arrayBuffer())
-  let txt = `Prueba
-
-prueba 
-
-prueba`
-await conn.sendMessage(m.chat, { text: txt, contextInfo:{ forwardingScore: 999, isForwarded: false,  "externalAdReply": { "showAdAttribution": false, "containsAutoReply": false, title: `PRUEBA`, body: textbot, "previewType": "PHOTO", thumbnailUrl: thumb, sourceUrl: null }}}, { quoted: m})
-}
+await conn.sendMessage(m.chat, { text: respuesta, 
+contextInfo: { forwardingScore: 1, isForwarded: false, 
+externalAdReply: { showAdAttribution: false, renderLargerThumbnail: false, title: "Prueba", body: botname, containsAutoReply: true, mediaType: 1, thumbnailUrl: thumb, sourceUrl: null }}}, { quoted: m })
+await m.react("✅")
+} catch (error) {
+conn.sendMessage(m.chat, { text: `${error.message}` }, { quoted: m })
+}}
 
 handler.command = ["prueba"]
 export default handler
+
 
 
