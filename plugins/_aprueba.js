45ac68750fb3fbc262ceb061ff232e966acd5ffc
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-22 21:43:39 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -1,10 +1,12 @@
+import fetch from 'node-fetch' 
 let handler = async (m, { conn, command, usedPrefix }) => {
-let txt = `Prueba
+const thumb = Buffer.from(await (await fetch(`${global.toruImg}`)).arrayBuffer())
+  let txt = `Prueba
 
 prueba 
 
 prueba`
-await conn.sendMessage(m.chat, { text: txt, contextInfo:{ forwardingScore: 9999999, isForwarded: false,  "externalAdReply": { "showAdAttribution": true, "containsAutoReply": true, title: `PRUEBA`, body: textbot, "previewType": "PHOTO", thumbnailUrl: 'https://qu.ax/EQTd.jpg', sourceUrl: botweb }}}, { quoted: fkontak})
+await conn.sendMessage(m.chat, { text: txt, contextInfo:{ forwardingScore: 9999999, isForwarded: false,  "externalAdReply": { "showAdAttribution": true, "containsAutoReply": true, title: `PRUEBA`, body: textbot, "previewType": "PHOTO", thumbnailUrl: thumb, sourceUrl: botweb }}}, { quoted: m})
 }
 
 handler.command = ["prueba"]
