fc1b4a81efcfd3855ccfa7215db8d46f554ba694
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-25 18:25:31 -0300

    Update _aprueba.js

--- plugins/_aprueba.js
+++ plugins/_aprueba.js
@@ -1,1 +1,27 @@
+import axios from 'axios'
+import fetch from 'node-fetch'
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+if (!global.db.data.chats[m.chat].fAis && m.isGroup) {
+return conn.sendMessage(m.chat, { text: `📍  Los comandos de *[ inteligencia artificial ]* estan desactivados...` }, { quoted: m })
+}
 
+if (!text) return conn.sendMessage(m.chat, { text: `ᗢ Proporcione un texto para generar una cancion tipo *[ Romantica ]*.\n\n\t⚶ Por ejemplo:\n*${usedPrefix + command}* Eres la mas hermosa que he visto al nacer en este mundo.` }, { quoted: m })
+await m.react("⏰")
+try {
+let data = await fetch(`https://api.soymaycol.icu/sunoai?prompt=${text}&tags=romantic&apikey=soymaycol%3C3`)
+let toru = await data.json()
+
+if (!toru?.status || !toru?.music_url) {
+return conn.sendMessage(m.chat, { text: `📍  La api no obtuvo respuestas, intentalo en un minuto...` }, { quoted: m })
+}
+
+await conn.sendMessage(m.chat, { text: `· ┄ · ⊸ 𔓕 *Music  :  AI*\n\n\t＃ *Prompt* : ${text}\n\t＃ *Genero* : Romántico\n\t＃ *Mensaje* : Lyrics\n\n${toru.lyrics}\n\n> ${textbot}` }, { quoted: m })
+await conn.sendMessage(m.chat, { audio: { url: toru.music_url } }, { quoted: m })
+await m.react("✅")
+} catch (error) {
+conn.sendMessage(m.chat, { text: `${error.message}` }, { quoted: m })
+}}
+
+handler.command = ["romantic"]
+export default handler
+  
