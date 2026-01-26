8634cbabf9c562c73b838563e5b80f682b767fdb
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-25 22:15:09 -0300

    Add files via upload

--- plugins/rw-value.js
+++ plugins/rw-value.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix, command, text }) => {
-let codeA = "Comando en *[ mantenimiento ]*..."
-let codeB = "Comando en *[ eliminación ]*..."
-let codeC = "Comando de *[ desarrollo ]*..."
+let codeA = "〩 Comando en *[ mantenimiento ]*..."
+let codeB = "〩 Comando en *[ eliminación ]*..."
+let codeC = "〩 Comando en *[ desarrollo ]*..."
 await conn.sendMessage(m.chat, { text: codeC }, { quoted: m })
 }
 handler.command = ["value!"]
