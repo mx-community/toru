53590ff0e30a165b68fcfac777a32627b7aaec67
MX-DIGITAL <emprendejosuev@gmail.com>
2026-01-25 22:11:33 -0300

    Add files via upload

---
+++ plugins/rw-value.js
@@ -0,0 +1,8 @@
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+let codeA = "Comando en *[ mantenimiento ]*..."
+let codeB = "Comando en *[ eliminación ]*..."
+let codeC = "Comando de *[ desarrollo ]*..."
+await conn.sendMessage(m.chat, { text: codeC }, { quoted: m })
+}
+handler.command = ["value!"]
+export default handler
